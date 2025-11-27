2c2126387ecc30a6d019db4167e6bcec64522b92
Byron <bigzybyron@gmail.com>
2025-11-27 16:57:21 +0000

    Cookie Clicker Bot

--- Day 48 Selenium/main.py
+++ Day 48 Selenium/main.py
@@ -2,29 +2,30 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from pprint import pprint
+import time
 
 #Keep brower open
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-URL = "https://www.python.org/"
+URL = "https://ozh.github.io/cookieclicker/"
 driver.get(url=URL)
 
-events = driver.find_element(By.XPATH, value="/html/body/div/div[3]/div/section/div[3]/div[2]/div/ul").find_elements(By.TAG_NAME, value="li")
+time.sleep(5)
+driver.find_element(By.ID, value="langSelect-EN").click()
 
-event_dict = {}
+time.sleep(3)
+cookie = driver.find_element(By.ID, value="bigCookie")
 
-for index, event in enumerate(events):
-    date = event.find_element(By.TAG_NAME, value="time").get_attribute("datetime").split("T")[0]
-    name = event.find_element(By.TAG_NAME, value="a").text
+while True:
 
-    event_data = {
-        "date":date,
-        "name":name
-    }
-    event_dict[index] = event_data
+    available_upgrades = driver.find_elements(By.CSS_SELECTOR, value=".product.unlocked.enabled")
 
-pprint(event_dict)
+    if available_upgrades:
+        most_expensive = available_upgrades[-1]
+        most_expensive.click()
+    else:
+        cookie.click()
 
 driver.quit()
